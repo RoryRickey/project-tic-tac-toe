4d192ed65ad3e6861542eb35afc980349fabb06e
Rory Rickey <rrickey@ucsd.edu>
2019-01-29 21:09:33 +0000

    Added Tic Tac Toe (board and def code)

--- session_1_exercise.py
+++ session_1_exercise.py
@@ -3,14 +3,26 @@ import random
 def draw_board(board):
     # This function prints out the board that it was passed and should not return anything
     #HINT: pass a list to this function that holds the position of 'X's and 'O's
+    print(board[6] + "  | " + board[7] + " | " + board[8])
+    print("___|___|___")
+    print(board[3] + "  | " + board[4] + " | " + board[5])
+    print("___|___|___")
+    print(board[0] + "  | " + board[1] + " | " + board[2])
+    print("   |   |   ")
     return
 
 def input_player_letter():
+  print ("Please Select X or O")
+  player_letter = input()
+  player_letter = player_letter.upper()
+  while (player_letter!= ('X') and  player_letter!= ('O')):
+    print ("Please Select X or O")
+    player_letter = input()
+    player_letter = player_letter.upper()
     #Ask the player if they want to be 'X' or 'O'
     #Return the chosen letter
     #Hint: Use a loop to keep prompting the player for a letter if the input is not valid
-
-    return
+  return player_letter
 
 def who_goes_first():
     #Randomly choose who goes first
@@ -30,7 +42,7 @@ def main():
 
 
     #initialize your board list here to pass to draw the board
-    #board =
+    board = [' ']*9
     draw_board(board)
 
 main()
